746c12855b37e302b22af45b67cda76773a80073
matth2k <mrh259@cornell.edu>
2023-05-09 03:54:05 -0400

    update for better cloud

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -f words.txt
-find demos -maxdepth 2 -type f -name "*.dem" | xargs -t -n1 ./cs-word-cloud -input >> words.txt
-wordcloud_cli --width 2000 --height 4000 --background white --stopwords blacklist.txt --text words.txt --imagefile wordcloud.png
+find demos -maxdepth 2 -type f -name "*.dem" | xargs -t -n1 ./cs-word-cloud -delimiter ', ' -input >> words.txt
+wordcloud_cli --width 2000 --height 4000 --no_collocations --background white --stopwords blacklist.txt --text words.txt --imagefile wordcloud.png
