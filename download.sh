c9853bc3d7d884762377e5d3dea4ec30d8aacdbc
matth2k <mrh259@cornell.edu>
2023-05-10 00:20:53 -0400

    check other domains for files

--- download.sh
+++ download.sh
@@ -1,10 +1,24 @@
-#!/bin/bash -e
+#!/bin/bash
 
 ARCHIVE=$1-1-1.dem.gz
 DEMO=$1-1-1.dem
 NAME=$1
-wget -N --server-response https://storage.googleapis.com/demos-us-central1.faceit-cdn.net/csgo/$ARCHIVE 2>&1 >/dev/null | grep "HTTP/1.1 200\|HTTP/1.1 304" 
-echo "File downloaded"
+cat ../cdns.txt | while read line 
+do
+  wget -N --server-response $line/csgo/$ARCHIVE 2>&1 >/dev/null | grep "HTTP/1.1 200\|HTTP/1.1 304"
+  if [ $? -eq 0 ]; then
+    echo "File downloaded"
+    break
+  fi
+done
+
+if [ -f "$ARCHIVE" ]; then
+  echo "Demo downloaded"
+else 
+  echo "Skipping demo"
+  exit 1
+fi
+
 gzip -f -d $ARCHIVE
 ../cs-word-cloud -delimiter ', ' -input $DEMO > ${NAME}_words.txt
 ../weapon -delimiter ', ' -input $DEMO > ${NAME}_weapons.txt
