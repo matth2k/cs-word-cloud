b8642e9c14c8e48d702d9feab16e529c63381d93
matth2k <mrh259@cornell.edu>
2023-05-10 00:03:42 -0400

    make scripts more robust

--- download.sh
+++ download.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 
-ARCHIVE=1-$1-1-1.dem.gz
-DEMO=1-$1-1-1.dem
+ARCHIVE=$1-1-1.dem.gz
+DEMO=$1-1-1.dem
 NAME=$1
-wget https://storage.googleapis.com/demos-us-central1.faceit-cdn.net/csgo/$ARCHIVE
+wget -N --server-response https://storage.googleapis.com/demos-us-central1.faceit-cdn.net/csgo/$ARCHIVE 2>&1 >/dev/null | grep "HTTP/1.1 200\|HTTP/1.1 304" 
+echo "File downloaded"
 gzip -f -d $ARCHIVE
 ../cs-word-cloud -delimiter ', ' -input $DEMO > ${NAME}_words.txt
 ../weapon -delimiter ', ' -input $DEMO > ${NAME}_weapons.txt
